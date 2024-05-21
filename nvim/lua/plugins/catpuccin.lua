00aa0d9555f22c0c7431bedec45dd759a63a6cc8
Bimarsha Khanal <khanalbimarsha7@gmail.com>
2024-05-21 17:05:25 +0545

    Clear old configs

--- nvim/lua/plugins/catpuccin.lua
+++ nvim/lua/plugins/catpuccin.lua
@@ -3,6 +3,16 @@ return{
 	name = "catppuccin",
 	priority = 999,
 	config = function()
+		require("catppuccin").setup {
+			flavour = "mocha",
+			transparent_background = true,
+		    custom_highlights = function(colors)
+		        return {
+		            LineNr = { fg = colors.mauve },
+		        }
+		    end
+			}
 		vim.cmd.colorscheme "catppuccin"
+
 	end		
 }
